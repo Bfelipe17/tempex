f701ba140f5d5b72a6d157acabee633df71211f6
Bruno Felipe <bruno.felipe17@hotmail.com>
2022-08-01 18:07:37 -0300

    feat: get the current weather for a given location

--- lib/weather.ex
+++ lib/weather.ex
@@ -14,4 +14,31 @@ defmodule Weather do
   def kelvin_to_celsius(kelvin) do
     (kelvin - 273.15) |> Float.round(1)
   end
+
+  def temperature_of(location) do
+    result = get_endpoint(location) |> HTTPoison.get() |> parse_response()
+
+    case result do
+      {:ok, temp} -> "#{location}: #{temp} ºC"
+      :error -> "#{location} not found"
+    end
+  end
+
+  defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+    body
+    |> JSON.decode!()
+    |> compute_temperature()
+  end
+
+  defp parse_response(_), do: :error
+
+  defp compute_temperature(%{"main" => %{"temp" => temp}}) do
+    try do
+      temp |> kelvin_to_celsius()
+
+      {:ok, temp}
+    rescue
+      _ -> :error
+    end
+  end
 end
