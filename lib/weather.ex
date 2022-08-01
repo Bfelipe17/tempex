4d6dd04d4bf002dd81da7f5fbe4d9ab52b72cba8
Bruno Felipe <bruno.felipe17@hotmail.com>
2022-08-01 18:37:19 -0300

    feat: taking the temperature in parallel

--- lib/weather.ex
+++ lib/weather.ex
@@ -1,6 +1,51 @@
 defmodule Weather do
   @api "http://api.openweathermap.org/data/2.5/weather?q="
 
+  def start(cities) do
+    manager_pid = spawn(__MODULE__, :manager, [[], Enum.count(cities)])
+
+    cities
+    |> Enum.map(fn city ->
+      pid = spawn(__MODULE__, :get_temperature, [])
+      send(pid, {manager_pid, city})
+    end)
+  end
+
+  def manager(cities \\ [], total) do
+    receive do
+      {:ok, temp} ->
+        results = [temp | cities]
+
+        if(Enum.count(results) == total) do
+          send(self(), :exit)
+        end
+
+        manager(results, total)
+
+      :exit ->
+        IO.puts(cities |> Enum.sort() |> Enum.join(", "))
+
+      _ ->
+        manager(cities, total)
+    end
+  end
+
+  def get_temperature() do
+    receive do
+      {manager_pid, location} ->
+        send(manager_pid, {:ok, temperature_of(location)})
+
+      _ ->
+        IO.puts("Error")
+    end
+
+    get_temperature()
+  end
+
+  def display_city(city) do
+    IO.inspect(city)
+  end
+
   def get_appid() do
     System.get_env("API_KEY")
   end
